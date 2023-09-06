c6828aae9bea97b66012225622a7acab06237a49
Damla Kilinc <dddmlklnc@gmail.com>
2023-09-06 22:47:02 +0300

    first commit

---
+++ src/CiftveDordunKatlarininToplami.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CiftveDordunKatlarininToplami {
+    public static void main(String[] args) {
+        int n,total=0;
+        Scanner scan=new Scanner(System.in);
+        do{
+            System.out.print("Sayı giriniz:");
+            n=scan.nextInt();
+            if(n%4 == 0){
+                total+=n;
+            }
+        }while(n%2==0);
+        System.out.print("Toplam: "+total);
+    }
+}
